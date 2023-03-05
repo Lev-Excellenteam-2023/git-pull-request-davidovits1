1ddd33f8b1a5a95732b4f33e8517d743fad31ff2
davidovits1 <david.o3111@gmail.com>
2023-03-05 17:21:04 +0200

    finish exce 5.4.

--- 5.4.connected_vessels.py
+++ 5.4.connected_vessels.py
@@ -3,10 +3,21 @@ def interleave(*argu):
     for i in range(len(argu)):
         for item in argu:
             flat_list.append(item[i])
+    return flat_list
 
-    yield flat_list
+
+def generator_interleave(*argu):
+    flat_list = []
+    for i in range(len(argu)):
+        for item in argu:
+            flat_list.append(item[i])
+            yield flat_list
 
 
-our_generator = interleave('abc', [1, 2, 3], ('!', '@', '#'))
+print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+
+print("\n##### generator_interleave ####\n")
+
+our_generator = generator_interleave('abc', [1, 2, 3], ('!', '@', '#'))
 for item in our_generator:
     print(item)
